1d75173e56f10efce431b4bdb6497b1f077eb45a
Adonay Veiga <adonayveiga@gmail.com>
2020-11-30 18:08:09 -0300

    Let's drink

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, except: %i[update destroy edit]
+  resources :cocktails, except: %i[update destroy edit] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: :destroy
 end
